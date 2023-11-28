21d1cdde3db28026eaecb0eb9826d5b9e4c2ada2
Logan Lechuga <loganlechuga@gmail.com>
2023-11-27 22:12:18 -0500

    Commit #1

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "327 Final Project Calorie Tracker"
+include(":app")
