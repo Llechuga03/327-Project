59a27f1b3b0dd753778d5ea0bacd9bb652fd73f1
Angel Amaya <135993385+aamaya33@users.noreply.github.com>
2023-11-30 19:24:38 -0500

    Food API implementation
    Began development for the user to be able to submit queries into the FoodData Central API, very much a skeleton for now. Created corrisponding classes that will be able to hold the information for the GET request.
    
    FIXME:
    Need to fully implement the food search. Right now it has a good amount of errors so that will need some debugging and some time.
    
    Once we're able to make successful calls and get the information we should be all good. Here is some documentation I've been looking at to help me with this process
    
    https://app.swaggerhub.com/apis/fdcnal/food-data_central_api/1.0.1#/servers
    
    to look at the table schemas so that we can get the labelNutrients field from the BrandedFoodItem Schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    //Fix Kotlin duplicate class error
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation ("androidx.appcompat:appcompat:1.3.0")
+//    implementation("com.android.car.ui:car-ui-lib:2.5.1")
+    //Install Retrofit to make API calls
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
