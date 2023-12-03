0712671c3c715d1f194848c25df6a3f3c8f53667
Llechuga03 <140262648+Llechuga03@users.noreply.github.com>
2023-12-02 22:19:50 -0500

    test progress bars

--- app/src/main/java/com/example/a327finalprojectcalorietracker/Add_Meal.java
+++ app/src/main/java/com/example/a327finalprojectcalorietracker/Add_Meal.java
@@ -2,7 +2,7 @@ package com.example.a327finalprojectcalorietracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.widget.AdapterView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,21 +25,21 @@ import retrofit2.http.Query;
 
 
 public class Add_Meal extends AppCompatActivity {
-    String api_key;
+    String api_key = "fMoh8eI1l06bnM1emwJx5DWxDU9AwY3AMNHfXeSp";
 
     //these two should increase the progress bar on the home page as the user
     //click on what food they're entering from the search bar
-    private double calculateConsumedCalories(labelNutrients nutritionalInfo){
+    private double calculateConsumedCalories(labelNutrients nutritionalInfo) {
         //Calculate consumed calories based on the nutritional info received
         double calories = nutritionalInfo.getCalories();
 
         return calories;
     }
 
-    private void updateProgressBar(double consumedCalories){
+    private void updateProgressBar(double consumedCalories) {
         double maxCalories = 2000;
 
-        int progress=(int) ((consumedCalories/maxCalories)*100);
+        int progress = (int) ((consumedCalories / maxCalories) * 100);
 
         updateProgressBar(progress);
     }
@@ -61,21 +61,17 @@ public class Add_Meal extends AppCompatActivity {
 
         // Make sure the SearchView is able to Read Queries
         SearchView searchView = findViewById(R.id.searchViewButton);
-        ListView listView = findViewById(R.id.listView);
-        //Just added this, not sure if this will intefere with the one that's already defined inside one of the other override fields
-
         // Set an OnQueryTextListener to the SearchView
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
-        {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit (String query){
+            public boolean onQueryTextSubmit(String query) {
                 // This method is called when the user submits the query (presses Enter)
                 Log.d("SearchView", "Query submitted: " + query);
                 return true;
             }
 
             @Override
-            public boolean onQueryTextChange (String newText){
+            public boolean onQueryTextChange(String newText) {
                 // This method is called when the text in the search view changes
                 Log.d("SearchView", "Query text changed: " + newText);
 
@@ -116,87 +112,56 @@ public class Add_Meal extends AppCompatActivity {
                 });
 
 
-
                 return true;
+            }
+        });
+
+        // Set the onItemClickListener
+        UserFoods userFoods = new UserFoods();
+
+        ListView listView = findViewById(R.id.listView);
+
+        List<foodItem> foodItems = userFoods.getFoodItems();
+
+        ArrayAdapter<foodItem> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foodItems);
+
+        listView.setAdapter(adapter);
+
+        //FIXME appending information makes app crash @line 118 || parent.getItemAtPosition(position) is returning a String
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item from ListView
+                foodItem selectedItem = (foodItem) parent.getItemAtPosition(position);
+
+                // Check if the selected item is not null
+                if (selectedItem != null) {
+                    // Call addFood on the instance of UserFoods
+                    userFoods.addFood(selectedItem);
+
+                    // Log the updated UserFoods
+                    Log.d("UserFoods", "Updated UserFoods: " + userFoods.toString());
+                    //Calculate consumed calories here based on the selected item's nutritonal info
+                    double consumedCalories = calculateConsumedCalories(selectedItem.getLabelNutrients());
+
+                    //create an Intent to send progress values back to MainActivity
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra("consumedCalories",consumedCalories);
+                    setResult(RESULT_OK, resultIntent);
+                    finish(); //Finish Add_Meal activity and return to MainActivity
+                } else {
+                    Log.e("UserFoods", "Selected food item is null");
+                }
             }
         });
     }
 
-//    private void performFoodSearch(String query) {
-//        Call<foodItem> call = FoodSearchService.getBrandedFoodItem(query, "your_api_key");
-//        call.enqueue(new Callback<APIResponse>() {
-//            @Override
-//            public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
-//                if (response.isSuccessful() && response.body() != null) {
-//                    List<foodItem> foodItems = response.body().getFoods();
-//                    if (foodItems != null && !foodItems.isEmpty()) {
-//                        // For simplicity, let's just take the first food item
-//                        foodItem firstFoodItem = foodItems.get(0);
-//                        labelNutrients nutrients = firstFoodItem.getLabelNutrients();
-//                        // Now you can use the 'nutrients' object to access the label nutrients data
-//                        // Accessing nutrient values
-//                        double fat = nutrients.getFat();
-//                        double saturatedFat = nutrients.getSaturatedFat();
-//                        double transFat = nutrients.getTransFat();
-//                        double cholesterol = nutrients.getCholesterol();
-//                        double sodium = nutrients.getSodium();
-//                        double carbohydrates = nutrients.getCarbohydrates();
-//                        double fiber = nutrients.getFiber();
-//                        double sugars = nutrients.getSugars();
-//                        double protein = nutrients.getProtein();
-//                        double calcium = nutrients.getCalcium();
-//                        double iron = nutrients.getIron();
-//                        double potassium = nutrients.getPotassium();
-//                        double calories = nutrients.getCalories();
-//
-//                        //Print them in the console
-//                        printNutrientValues(fat, saturatedFat, transFat, cholesterol, sodium, carbohydrates,
-//                                fiber, sugars, protein, calcium, iron, potassium, calories);
-//                    } else {
-//                        // Handle the case when no food items are found
-//                        Toast.makeText(Add_Meal.this, "No results found", Toast.LENGTH_SHORT).show();
-//                    }
-//                } else {
-//                    // Handle the case when the API call is not successful
-//                    Toast.makeText(Add_Meal.this, "Error in API call", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<APIResponse> call, Throwable t) {
-//                // Handle the case when the API call fails
-//                Toast.makeText(Add_Meal.this, "Failed to retrieve data", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
 
+        public void goToMainMenu (View view){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
 
-    private void printNutrientValues(double fat, double saturatedFat, double transFat, double cholesterol,
-                                     double sodium, double carbohydrates, double fiber, double sugars,
-                                     double protein, double calcium, double iron, double potassium,
-                                     double calories) {
-        // Example: Print nutrient values to the console
-        System.out.println("Nutrient Values:");
-        System.out.println("Fat: " + fat + " grams");
-        System.out.println("Saturated Fat: " + saturatedFat + " grams");
-        System.out.println("Trans Fat: " + transFat + " grams");
-        System.out.println("Cholesterol: " + cholesterol + " milligrams");
-        System.out.println("Sodium: " + sodium + " milligrams");
-        System.out.println("Carbohydrates: " + carbohydrates + " grams");
-        System.out.println("Fiber: " + fiber + " grams");
-        System.out.println("Sugars: " + sugars + " grams");
-        System.out.println("Protein: " + protein + " grams");
-        System.out.println("Calcium: " + calcium + " milligrams");
-        System.out.println("Iron: " + iron + " milligrams");
-        System.out.println("Potassium: " + potassium + " milligrams");
-        System.out.println("Calories: " + calories + " calories");
-    }
-
-    public void goToMainMenu(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    // Make an API call to retrieve nutritional information for the consumed food
-
+        // Make an API call to retrieve nutritional information for the consumed food
 }
+
